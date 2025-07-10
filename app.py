61f76e24cf42af38e09bc5c27b39abf3927ac404
eduardo130796 <82067640+eduardo130796@users.noreply.github.com>
2025-07-09 23:45:55 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(page_title="CCONT", page_icon="🗂️", layout="wide")
+# --- Sidebar ---
+
+
+    
+
+pages = {
+    "Pagina Inícial":[
+        st.Page("pages/index.py", title="Início", icon="🏠"),
+    ],
+    "Relatórios": [
+        st.Page("pages/app_contratos.py", title="Painel Contratos", icon="📊"),
+        st.Page("pages/orcame.py", title="Painel Orçamentário", icon="📊"),
+    ],
+}
+pg = st.navigation(pages)
+pg.run()
+
+with st.sidebar:
+    # Configuração da página
+    st.caption("© 2025 - Eduardo Júnior")
